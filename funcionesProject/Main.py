9d031a1ce141dbd402e4ffd0c681f8ba90715ebf
ROSBY ALICIA PAZ JIMENEZ <1061806891@fup.edu>
2024-10-03 18:57:08 -0500

    Se agrega el archiv main

---
+++ funcionesProject/Main.py
@@ -0,0 +1,18 @@
+def my_function():
+    print("Hello from a function")
+print("Hoy es el quizz")
+    #Invocar la funcion
+my_function()
+
+
+#Ejemplo funcion con argumentos
+
+def mostrarNombre(nombre):
+    print("su nombre es: "+nombre)
+
+nom = input("ingrese su nombre: ")
+#Invoca la funcion
+mostrarNombre(nom)
+
+
+
